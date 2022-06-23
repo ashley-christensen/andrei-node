d37080eaa8c5a9cf9d08ab1b930ea384edd3d628
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-06-22 17:26:11 -0700

    xhr object

--- test.js
+++ test.js
@@ -21,3 +21,20 @@ promise
  .then(result2 => result2 + '?')
  .catch(() => console.log('errorr'))
  .then(result3 => console.log(result3 + '!'));
+
+const promise2 = new Promise((resolve, reject) => {
+ setTimeout(resolve, 100, 'HIII');
+});
+
+const promise3 = new Promise((resolve, reject) => {
+ setTimeout(resolve, 1000, 'POOKIE');
+});
+
+const promise4 = new Promise((resolve, reject) => {
+ setTimeout(resolve, 5000, 'What is up');
+});
+
+Promise.all([promise, promise2, promise3, promise4])
+ .then(values => {
+  console.log(values);
+ });

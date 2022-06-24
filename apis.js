50448aef724f1c6d800fa66ec71cda1a768a3ba4
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-06-24 11:30:24 -0700

    fetch txt file

--- apis.js
+++ apis.js
@@ -1,14 +1,21 @@
+
+
+// Promise.all(urls.map(url => {
+//  return fetch(url).then(response => response.json());
+// }
+//  )).then(results => {
+//   console.log(results[0])
+//   console.log(results[1])
+//   console.log(results[2])
+//  })
 const urls = [
  'https://jsonplaceholder.typicode.com/users',
  'https://jsonplaceholder.typicode.com/posts',
  'https://jsonplaceholder.typicode.com/albums'
-]
+];
 
-Promise.all(urls.map(url => {
- return fetch(url).then(response => response.json());
-}
- )).then(results => {
-  console.log(results[0])
-  console.log(results[1])
-  console.log(results[2])
- })
+const getInfo = Promise.all(urls.map(url => {
+ return fetch(url).then(res => res.json())
+})).then(results => {
+ console.log(results)
+})

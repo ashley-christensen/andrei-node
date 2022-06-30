719d9e61b5ef428a779c077c77c63147683b47be
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-06-30 11:14:43 -0700

    modules req/res exports

---
+++ MODULES-EXAMPLE/response.js
@@ -0,0 +1,9 @@
+function decrypt(data) {
+ //library that implements TLS, protocol browsers/servers use to send encrypt. data over web
+ return 'decrypted data';
+}
+
+exports.read = function read() {
+ return decrypted = decrypt('data')
+}
+

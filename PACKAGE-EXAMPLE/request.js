bbd465ab5afd8ce3f9d3e698d94eb99129c6edbf
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-07-01 11:07:55 -0700

    axios then, catch, then

--- PACKAGE-EXAMPLE/request.js
+++ PACKAGE-EXAMPLE/request.js
@@ -1,1 +1,12 @@
 const axios = require('axios');
+
+axios.get('http://wwwwww.google.com')
+ .then((response) => {
+   console.log(response);
+  })
+  .catch((error) => {
+   console.log(error)
+  })
+  .then(() => {
+   console.log('this execute regardless!!!!!!')
+  })
